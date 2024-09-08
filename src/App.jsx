2dca8e7d71b2df92fbbf5e9b7a837d8190f5c7ae
Stefan Sabev <stevenbg91@gmail.com>
2024-09-08 10:46:11 +0200

    minor updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import Footer from "./components/footer/Footer"
+import HeroBanner from "./components/heroBanner/HeroBanner"
+import Home from "./components/home/Home"
+import { useGetProducts } from "./hooks/useGetProducts"
+
 
 function App() {
- 
+
+  const {products,banner,isLoading} = useGetProducts()
+  console.log(products)
+
 
   return (
     <>
-
+    {isLoading && products && <div>Loading...</div> }
+    <HeroBanner />
+      Hero Banner
+      <Home />
+     <Footer />
     </>
   )
 }
