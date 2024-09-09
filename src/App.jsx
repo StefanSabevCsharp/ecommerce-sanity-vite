ef3af8db3ed19d9b3f9b23e25bb845da0b22ba92
Stefan Sabev <stevenbg91@gmail.com>
2024-09-09 18:52:34 +0200

    Layout adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
 import Footer from "./components/footer/Footer"
+import FooterBanner from "./components/footerBanner/FooterBanner"
 import HeroBanner from "./components/heroBanner/HeroBanner"
 import Home from "./components/home/Home"
+import NavBar from "./components/navbar/Navbar"
 import { useGetProducts } from "./hooks/useGetProducts"
 
 
 function App() {
 
-  const {products,banner,isLoading} = useGetProducts()
+  const { products, banner, isLoading } = useGetProducts()
   console.log(products)
 
 
   return (
     <>
-    {isLoading && products && <div>Loading...</div> }
-    <HeroBanner />
-      Hero Banner
-      <Home />
-     <Footer />
+      <div className="layout">
+        <header>
+          <NavBar />
+        </header>
+        <main className="main-container">
+          {isLoading ? <div>Loading...</div> :
+            (<>
+              <HeroBanner banner={banner[0]} />
+              <Home products={products} />
+              <FooterBanner banner={banner[0]} />
+            </>
+            )}
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+
     </>
   )
 }
