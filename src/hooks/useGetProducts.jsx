9f1e75e17ca6e5bb1c5413699bdd69c5ebb49211
Stefan Sabev <stevenbg91@gmail.com>
2024-09-14 15:08:22 +0200

    Add new custom hook

--- src/hooks/useGetProducts.jsx
+++ src/hooks/useGetProducts.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { fetchBanners, fetchProducts } from "../dataService/productsService"
+import { fetchBanners, fetchProducts, fetchSingleProduct } from "../dataService/productsService"
 
 export const useGetProducts = () => {
     const [products, setProducts] = useState([])
@@ -27,4 +27,27 @@ export const useGetProducts = () => {
     return { products, banner, isLoading }
 
 
+}
+
+export const useGetSingleProduct = (slug) => {
+    const [product, setProduct] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        setIsLoading(true)
+        try {
+            const getData = async () => {
+                const productsData = await fetchSingleProduct(slug);
+                console.log(productsData)
+                setProduct(productsData);
+                setIsLoading(false)
+            }
+            getData();
+        } catch (error) {
+            console.error(error)
+        }
+
+    }, [slug])
+
+    return [product, isLoading]
 }
