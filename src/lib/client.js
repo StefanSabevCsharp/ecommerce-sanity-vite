76a9e55016e160329d68516cb6d24a1a246c9c4f
Stefan Sabev <stevenbg91@gmail.com>
2024-09-16 09:15:19 +0200

    Update client.js

--- src/lib/client.js
+++ src/lib/client.js
@@ -2,9 +2,9 @@ import {createClient} from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-    projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
-    dataset: import.meta.env.VITE_SANITY_DATASET,
-    apiVersion: import.meta.env.VITE_SANITY_API_VERSION,
+    projectId: import.meta.env.VITE_SANITY_PROJECT_ID || "69060ayt",
+    dataset: import.meta.env.VITE_SANITY_DATASET || "production",
+    apiVersion: import.meta.env.VITE_SANITY_API_VERSION || "2021-10-21",
     useCdn: true,
     token: import.meta.env.VITE_SANITY_TOKEN
 })
